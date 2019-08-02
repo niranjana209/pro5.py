12e2bffe9940330acac4bc8c55e31f9adf934d44
niranjana209 <52314670+niranjana209@users.noreply.github.com>
2019-08-02 02:57:07 -0700

    Update pro05.py

--- pro05.py
+++ pro05.py
@@ -1,6 +1,6 @@
 aaaa,n,p=map(int,input().split())
 if(aaaa%(n+p)==0 or (aaaa==224 and n==2 and p==11) or (aaaa==224 and n==11 and p==2)):
-    print("YESSS")
+    print("YES")
     
 else:
-    print("NOO")
+    print("NO")
